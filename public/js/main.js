c7a3daf7ad96ecb985b76bc846ec24d3015e0736
Shubhra.Prakash <prakashnitza@gmail.com>
2018-07-18 11:56:32 -0400

    added comments and cleaned up. Added Notes on README.md

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,44 @@
+//Function that runs when document is loaded.
 document.addEventListener("DOMContentLoaded", function(event) { 
-	var favfilmCount=0;
+	
+	var favfilmCount=0; //global variable keeping track of number of favs. 
 
+	//when user clicks search thereby submitting the form
 	document.getElementById('movieform').addEventListener('submit', function(evt){
     	evt.preventDefault();
-		console.log('submit now');
 
+    	//create ajax request to api for movie names
 		var xhr = new XMLHttpRequest();
 		var params = "moviename="+document.getElementById('moviename').value;
 		xhr.open('GET', '/getMovieList?'+params, true);
 		xhr.setRequestHeader('Content-Type', 'application/json');
 
+		//loading response
 		xhr.onload = function() {
+
+			//element where movie results will be written
 			const results = document.querySelector("#results");
   			results.innerHTML = "";
 
+  			//if request is a success.
 		    if (xhr.status === 200) {
+
+		    	//parse the response
 		    	var movieResults = JSON.parse(xhr.response);
 
 		    	//console.log(JSON.parse(xhr.response)[0].Title);
 
+		    	//we have at least one movie result returned.
 		    	if(movieResults.length > 0){
 		    		
+		    		//go through all the results
 		    		for (var i=0; i< movieResults.length; i++){
 
+		    			//create proper elements that will store the result.
 		    			var movieItemDiv = document.createElement('div');
 		    			var movieItemDetails = document.createElement('div');
 
+		    			//get the title of the film and write into HTML element for display
                         var titleLink = document.createElement("a"); 
                         titleLink.href = "#";
                         titleLink.innerHTML = (i+1)+". " + movieResults[i].Title;
@@ -35,20 +48,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
                         	this.parentElement.children[1].style.display="block";
                         });
 
+                        //get the year of the film and write into HTML element for display.
                         var movieYear = document.createElement('div');
                         movieYear.innerHTML = "Year : " + movieResults[i].Year;
 
+                        //get the imdbid of the film and write into HTML element for display.
                         var movieImdbID = document.createElement('div');
                         movieImdbID.innerHTML = "IMDB ID : " + movieResults[i].imdbID;
 
+                        //get the movietype of the film and write into HTML element for display.
                         var movieType = document.createElement('div');
                         movieType.innerHTML = "Type : " +movieResults[i].Type;
 
+                        //get the poster url of the film and write into HTML element for display.
                         var moviePoster = document.createElement('div');
                         moviePoster.innerHTML = 'Poster  <img src='+movieResults[i].Poster+'/>';
 
+                        //create radio button to mark movie as favorite
                         var movieFav = document.createElement('input');
-                        movieFav.type = 'checkbox';
+                        movieFav.type = 'radio';
                         movieFav.name = 'movieFave';
                         movieFav.checked = false;
 
@@ -57,7 +75,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
                         var movieFavSpan = document.createElement('span');
                         movieFavSpan.innerHTML = 'Mark as a Favorite Movie';
 
-                       
+                        //put all HTML elements together for display.
 		    			movieItemDetails.className = "movie-item-details";
                         movieItemDetails.appendChild(movieYear);
                         movieItemDetails.appendChild(movieImdbID);
@@ -76,63 +94,79 @@ document.addEventListener("DOMContentLoaded", function(event) {
                             
                  	 }   
 		    	}
-		    } else if (xhr.status === 500) {
-		    		results.innerHTML = "Sorry No Results For That Movie Name";
+		    } else if (xhr.status === 500) { // if response has an issue
+		    		results.innerHTML = "Sorry No Results For That Movie Name.";
 		    }
 		     else {
-		     		results.innerHTML = "Sorry No Results For That Movie Name";
-		    		console.log("Sorry No Results");
+		     		results.innerHTML = "Sorry No Results For That Movie Name.";
+		    		console.log("Sorry No Results.");
 		    }
 		};
 		xhr.send();
 	});
-
+	
+	// Event Handler for when Your Favorite Movies is clicked.
 	document.getElementById("fav-link").addEventListener("click", listFavorites, false);
 
+	//make an api call to post favorite movie data per radio button click
 	function markAsFav() {
+		
+		favfilmCount++; //keeping track of # of fav movies.
 
-		favfilmCount++;
-		var favfilm = this.parentElement.parentElement.children[0].innerHTML;
-		var favfilmTitle= favfilm.split(".")[1];
+		//format the content you want to show as fav movie. Title and Year (in case there are duplicates)
+		var favfilm = this.parentElement.parentElement.children[0].innerHTML; 
+		var favfilmTitle= favfilm.split(".")[1]; 
 
 		var favfilmYear = this.parentElement.parentElement.children[1].children[0].innerHTML;//in case of duplicates, you have year to distinguish
 		var setFavReq = new XMLHttpRequest();
 		var setFavUrl = '/setFavorites';
 
-		console.log(favfilmTitle + "json");
+		//construct a json data object that will contain favorite movies data which will be sent to api. 
 		var favJSONData = JSON.stringify({"fav" : favfilmTitle+" "+favfilmYear});
 
+		//make api call and send favorite movies data.
 		setFavReq.open('POST', '/setFavorites', true);
 		setFavReq.setRequestHeader('Content-Type', 'application/json');
 		setFavReq.send(favJSONData);
 		ocument.getElementById('favMovies').innerHTML = "";
 	}
 
+	//make an api call to list favorites, does not handle for duplicates if the film is made a favorite again by a repeated sarch.
 	function listFavorites() {
+
+		//make request to get favorited movies
 		var listFavsReq = new XMLHttpRequest();
 		listFavsReq.open('GET', '/favorites', true);
 		listFavsReq.setRequestHeader('Content-Type', 'application/json');
 
 		listFavsReq.onload = function() {
-			if (listFavsReq.status === 200) { 
+			if (listFavsReq.status === 200) {  //if request is success.
 
+				//parse the returned data
 				var favMovies = JSON.parse(listFavsReq.response);
+
+				//more than one favorite film has been returned.
 				if(favMovies.length > 0){
 
+					//format the content into HTML elements to be displayed as favorite movies. Title and Year (in case of duplicates).
 					var favMovieContainer = document.getElementById('favMovies');
 					favMovieContainer.innerHTML = "";
 
 		    		var favMovieList = document.createElement('ul');
 		    		favMovieList.className = "favMovieList";
 
+		    		//go through all the favorited movies
 		    		for (var i=0; i< favMovies.length; i++){
 						var favMovie = document.createElement('li');
 			   			favMovie.innerHTML = (i+1)+". "+favMovies[i].substring(1, favMovies[i].length-1);
 			   			favMovieList.appendChild(favMovie);
 
 		    		}
+
+		    		//render the data onto HTML element.
 		    		favMovieContainer.appendChild(favMovieList);
-		    	} else {
+
+		    	} else { //if request is not successful
 		    		document.getElementById('favMovies').innerHTML = "You have not yet marked any movies as favorites. Search and mark as favorite.";
 		    	}
 			}
--- server.js
+++ server.js
@@ -11,56 +11,66 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, 'public')))
 
+//API call to get a list of movies in OMDB based on the movie name entered. 
 app.get('/getMovieList', function(req,res){
   //console.log("req params" + req.query);
-  let moviename = req.query.moviename.trim();
-  if(moviename !== '') {
+
+  let moviename = req.query.moviename.trim(); //get user entered movie name that is passed in via get query params
+  if(moviename !== '') { //make sure movie name is not empty
+
+   //actual call to OMDB api 
    request.get("http://www.omdbapi.com/?s="+moviename+"&apikey=160ca515", (error, response, body) => {
+       
        if(error) {
-          return console.dir(error);
+          return console.dir(error); //handle if api call fails 
        } else {
-              let movieList = JSON.parse(body);
-              if(movieList['Response'] === 'True') {
-                  res.setHeader('Content-Type', 'application/json');
+              let movieList = JSON.parse(body); //get result and parse JSON data
+
+              if(movieList['Response'] === 'True') {  //if there are movies that are returned
+
+                  res.setHeader('Content-Type', 'application/json'); //write back result in JSON.
                 //  console.log(JSON.stringify(movieList['Search']));
-                  var json = JSON.stringify(movieList['Search']);
+                  var json = JSON.stringify(movieList['Search']);  //send all movies returned
                   res.end(json);
                 } else {
-                  res.status(500).send({ error: "No Film with that name in OMDB Search" });
+                  res.status(500).send({ error: "No Film with that name in OMDB Search" });  //in case no movies come back.
                 }
         }    
     });
   } else {
-    res.status(500).send({ error: "Movie Name not readable" });
+    res.status(500).send({ error: "Movie Name not readable" });  //if movie name is not present or readable. 
     console.log("Movie Name not readable");
   }
 
 });
 
+//api call to get all favorite movies 
 app.get('/favorites', function(req, res){
-  var data = fs.readFileSync('./data.json');
+  var data = fs.readFileSync('./data.json');  //read the data from a data.json file
   res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+  res.send(data); // send back the data from the file to middleware js.
 });
 
+//api call to write favorites to json file so it is persisted
 app.post('/setFavorites', function(req, res){
-  console.log(" check " + req.body.fav );
+ // console.log(" check " + req.body.fav );
   
-  if(!req.body.fav){
+  if(!req.body.fav){  //if params in the request don't have favorite set.
     res.send("Error");
     return
   }
   
-  var data = JSON.parse(fs.readFileSync('./data.json'));
+  var data = JSON.parse(fs.readFileSync('./data.json'));  // open json file to read
 
-  data.push("{"+req.body.fav+ "}");
+  data.push("{"+req.body.fav+ "}");  //push the data to variable
   
-  fs.writeFile('./data.json', JSON.stringify(data));
+  fs.writeFile('./data.json', JSON.stringify(data)); //write variable to file after converting to string
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
 });
 
 
+//Always listen on this port for this app.
 app.listen(3000, function(){
   console.log("Listening on port 3000");
 });
